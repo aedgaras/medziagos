24427c5565bb0fb9a4c0ac264fcacd0bf03e9cef
unknown <dominykasmurauskas1@gmail.com>
2021-12-06 23:04:21 +0200

    Refactor to functional react and add some functionality in homepage

--- src/pages/Equations.js
+++ src/pages/Equations.js
@@ -1,71 +1,64 @@
-import { Box, Center, Container, Stack, Text } from "@chakra-ui/layout";
+import {Box, Center, Container, Stack, Text} from "@chakra-ui/layout";
 import React from "react";
 import "katex/dist/katex.min.css";
-import { InlineMath, BlockMath } from "react-katex";
-import { motion } from "framer-motion";
+import {InlineMath, BlockMath} from "react-katex";
+import {motion} from "framer-motion";
 
-class Equations extends React.Component {
-    constructor(props){
-        super(props);
+export const Equations = () => {
+    document.title = "Formulės";
 
-        document.title = "Formules";
-    }
-  render() {
     const blockFormula = `\\frac{n!}{k!(n-k)!} = \\binom{n}{k}`;
 
     const vidutinisIlgejimoKoeficientoPaklaida =
-      `\\frac{\\Delta\\alpha}{\\alpha_v}=` +
-      `\\Bigg|\\frac{\\Delta{L} _0}{{L}_0}\\Bigg|` +
-      `+\\Bigg|\\frac{\\Delta{L} _1}{{L}_1}\\Bigg|` +
-      `+\\Bigg|\\frac{\\Delta{\\Big(t_1-t_0\\Big)}}{t_1-t_0}\\Bigg|`;
+        `\\frac{\\Delta\\alpha}{\\alpha_v}=` +
+        `\\Bigg|\\frac{\\Delta{L} _0}{{L}_0}\\Bigg|` +
+        `+\\Bigg|\\frac{\\Delta{L} _1}{{L}_1}\\Bigg|` +
+        `+\\Bigg|\\frac{\\Delta{\\Big(t_1-t_0\\Big)}}{t_1-t_0}\\Bigg|`;
 
     const grafikas = `\\Delta{L}=f(t-t_0)`;
 
     const vidutinisIlgejimoKoeficientas =
-      `\\Delta\\alpha\\cong\\alpha=` +
-      `\\frac{L_1-L_0}{L_0\\Big(t_1-t_0\\Big)}=` +
-      `\\frac{\\Delta{L}}{L_0\\Big(t_1-t_0\\Big)}`;
+        `\\Delta\\alpha\\cong\\alpha=` +
+        `\\frac{L_1-L_0}{L_0\\Big(t_1-t_0\\Big)}=` +
+        `\\frac{\\Delta{L}}{L_0\\Big(t_1-t_0\\Big)}`;
 
     return (
-      <Container>
-        <Stack borderWidth="1px" borderRadius="lg" p={4}>
-          <motion.div
-            whileHover={{
-              scale: 1.2,
-              transition: { duration: 0.33 },
-            }}
-          >
-            <Box borderWidth="1px" borderRadius="lg" bgColor="#ffff">
-              <Text align="center">Vidutine Ilgejimo Koeficiento Paklaida</Text>
-              <BlockMath math={vidutinisIlgejimoKoeficientoPaklaida} />
-            </Box>
-          </motion.div>
-          <motion.div
-            whileHover={{
-              scale: 1.2,
-              transition: { duration: 0.33 },
-            }}
-          >
-            <Box borderWidth="1px" borderRadius="lg" bgColor="#ffff">
-              <Text align="center">Medziagos pailgejimas nuo temperaturos</Text>
-              <BlockMath math={grafikas} />
-            </Box>
-          </motion.div>
-          <motion.div
-            whileHover={{
-              scale: 1.2,
-              transition: { duration: 0.33 },
-            }}
-          >
-            <Box borderWidth="1px" borderRadius="lg" bgColor="#ffff">
-              <Text align="center">Vidutinis Ilgejimo Koeficientas</Text>
-              <BlockMath math={vidutinisIlgejimoKoeficientas} />
-            </Box>
-          </motion.div>
-        </Stack>
-      </Container>
+        <Container>
+            <Stack borderWidth="1px" borderRadius="lg" p={4}>
+                <motion.div
+                    whileHover={{
+                        scale: 1.2,
+                        transition: {duration: 0.33},
+                    }}
+                >
+                    <Box borderWidth="1px" borderRadius="lg" bgColor="#ffff">
+                        <Text align="center">Vidutinė ilgėjimo koeficiento paklaida</Text>
+                        <BlockMath math={vidutinisIlgejimoKoeficientoPaklaida}/>
+                    </Box>
+                </motion.div>
+                <motion.div
+                    whileHover={{
+                        scale: 1.2,
+                        transition: {duration: 0.33},
+                    }}
+                >
+                    <Box borderWidth="1px" borderRadius="lg" bgColor="#ffff">
+                        <Text align="center">Medžiagos pailgėjimas nuo temperatūros</Text>
+                        <BlockMath math={grafikas}/>
+                    </Box>
+                </motion.div>
+                <motion.div
+                    whileHover={{
+                        scale: 1.2,
+                        transition: {duration: 0.33},
+                    }}
+                >
+                    <Box borderWidth="1px" borderRadius="lg" bgColor="#ffff">
+                        <Text align="center">Vidutinis ilgėjimo koeficientas</Text>
+                        <BlockMath math={vidutinisIlgejimoKoeficientas}/>
+                    </Box>
+                </motion.div>
+            </Stack>
+        </Container>
     );
-  }
 }
-
-export default Equations;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,57 +1,151 @@
-import { Box, Center, Container, Text } from "@chakra-ui/layout";
-import React from "react";
-import { FormControl, FormLabel } from "@chakra-ui/form-control";
-import { Input } from "@chakra-ui/input";
+import {Center, Container} from "@chakra-ui/layout";
+import React, {useEffect, useState} from "react";
+import {FormControl, FormLabel} from "@chakra-ui/form-control";
+import {Input} from "@chakra-ui/input";
 import materialData from "../data/Data";
+import {Button, Grid, GridItem, Select} from "@chakra-ui/react";
+import {
+    Chart,
+    ChartSeries,
+    ChartSeriesItem,
+    ChartCategoryAxis,
+    ChartCategoryAxisItem, ChartCategoryAxisTitle, ChartValueAxisTitle, ChartValueAxisItem, ChartValueAxis
+} from "@progress/kendo-react-charts";
+import '@progress/kendo-theme-default/dist/all.css';
+import {all, create} from "mathjs";
 
-class Home extends React.Component {
-  constructor(props) {
-    super(props);
-
+export const Home = () => {
     document.title = "Pagrindinis";
-    this.state = {
-      tempInput: 0,
-      deltaL: 100,
-      data: materialData,
-    };
-  }
 
-  handleTempChange(event) {
-    const value = event.target.value;
-    const tempInput = Number(value);
+    const [temperatureStep, setTemperatureStep] = useState(5);
+    const [initialLength, setInitialLength] = useState(3630);
+    const [materialCoefficient, setMaterialCoefficient] = useState(0);
+    const [initialTemperature, setInitialTemperature] = useState(26);
+    const [currentHeatedTemperature, setCurrentHeatedTemperature] = useState(initialTemperature);
+    const [goalTemperature, setGoalTemperature] = useState(70);
+    const [heating, setHeating] = useState(false);
+    const [lengths, setLengths] = useState([]);
 
-    console.log(tempInput);
+    useEffect(() => {
+        if (heating && currentHeatedTemperature < goalTemperature) {
+            setTimeout(() => {
+                let newTemperature = currentHeatedTemperature + temperatureStep;
+                setLengths([...lengths, {temperature: newTemperature, length: calculateDeltaT(newTemperature)}]);
+                setCurrentHeatedTemperature(newTemperature);
+            }, 500);
+        }
 
+        if (heating && currentHeatedTemperature >= goalTemperature) {
+            setHeating(false);
+        }
+    }, [heating, currentHeatedTemperature])
 
+    const startHeating = () => {
+        setCurrentHeatedTemperature(initialTemperature);
+        setLengths([]);
+        setHeating(true);
+    }
 
-    this.setState({
-      tempInput: tempInput,
-    });
-  }
+    const calculateDeltaT = (heatedTemperature) => {
+        // TODO suapvalina reiksmes, neina normaliai apskaiciuoti
+        // 3630*(10^-3)*(1+16.5*(10^-6)*44 - pavyzdys pagal musu labora, kai temp. skirtumas 44 laipsniai
 
-  changeSize() {
-    return <Box borderWidth="1px" borderRadius="lg" width={this.state.deltaL + "px"} bgColor="red">Yooo</Box>;
-  }
+        const math = create(all, {
+            number: 'BigNumber',
+            precision: 20
+        })
 
-  render() {
+        let L0 = math.multiply(initialLength, math.pow(10, -3));
+        let temperaturesDiff = math.subtract(heatedTemperature, initialTemperature);
+        let exactCoefficient = math.multiply(materialCoefficient, math.pow(10, -6));
+        let multiplicationInParentheses = math.multiply(exactCoefficient, temperaturesDiff);
+        console.log(L0);
+        console.log(temperaturesDiff);
+        console.log(exactCoefficient);
+        console.log(multiplicationInParentheses);
+
+        let result = math.multiply(L0, math.sum(1, multiplicationInParentheses));
+        console.log(result);
+        return result;
+    }
+
     return (
-      <Center>
-        <Container align="center">
-          <Text>Home Page</Text>
-          <FormControl>
-            <FormLabel>Temperatura</FormLabel>
-            <Input
-              type="text"
-              onChange={(event) => this.handleTempChange(event)}
-            />
-          </FormControl>
-          <Box>
-            {this.changeSize()}
-          </Box>
-        </Container>
-      </Center>
+        <Grid templateRows='repeat(2, 1fr)' templateColumns='repeat(10, 1fr)' gap={2}>
+            <GridItem colSpan={5} h='10'>
+                <Center>
+                    <Container align="center">
+                        <FormControl>
+                            <FormLabel>Pradinė temperatūra °C</FormLabel>
+                            <Input
+                                value={initialTemperature}
+                                disabled={heating && ('disabled')}
+                                type="number"
+                                onChange={(event) => setInitialTemperature(Number(event.target.value))}
+                            />
+                        </FormControl>
+                        <FormControl>
+                            <FormLabel>Galutinė temperatūra °C</FormLabel>
+                            <Input
+                                value={goalTemperature}
+                                disabled={heating && ('disabled')}
+                                type="number"
+                                onChange={(event) => setGoalTemperature(Number(event.target.value))}
+                            />
+                        </FormControl>
+                        <FormControl>
+                            <FormLabel>Temperatūros žingsnis °C</FormLabel>
+                            <Input
+                                value={temperatureStep}
+                                disabled={heating && ('disabled')}
+                                type="number"
+                                onChange={(event) => setTemperatureStep(Number(event.target.value))}
+                            />
+                        </FormControl>
+                        <FormControl>
+                            <FormLabel>Pradinis vamzdelio ilgis (mm)</FormLabel>
+                            <Input
+                                value={initialLength}
+                                disabled={heating && ('disabled')}
+                                type="number"
+                                onChange={(event) => setInitialLength(event.target.value)}
+                            />
+                        </FormControl>
+                        <FormControl>
+                            <FormLabel>Vamzdelio medžiaga</FormLabel>
+                            <Select
+                                disabled={heating && ('disabled')}
+                                onChange={(event) => setMaterialCoefficient(event.target.value)}
+                            >
+                                {materialData.map((material, index) => {
+                                    return (<option key={index} value={material.tempK}>{material.name}</option>);
+                                })}
+                            </Select>
+                        </FormControl>
+                        <FormControl>
+                            <Button disabled={heating && ('disabled')}
+                                    onClick={() => startHeating()}>Start</Button>
+                        </FormControl>
+                    </Container>
+                </Center>
+            </GridItem>
+            <GridItem colSpan={5}>
+                <Chart>
+                    <ChartValueAxis>
+                        <ChartValueAxisItem>
+                            <ChartValueAxisTitle text="Strypo ilgis mm"/>
+                        </ChartValueAxisItem>
+                    </ChartValueAxis>
+                    <ChartCategoryAxis>
+                        <ChartCategoryAxisItem categories={lengths.map((length) => length.temperature)}>
+                            <ChartCategoryAxisTitle text="Temperatūra"/>
+                        </ChartCategoryAxisItem>
+                    </ChartCategoryAxis>
+                    <ChartSeries>
+                        <ChartSeriesItem type="line" tooltip={{visible: true}}
+                                         data={lengths.map((length) => length.length)}/>
+                    </ChartSeries>
+                </Chart>
+            </GridItem>
+        </Grid>
     );
-  }
 }
-
-export default Home;
